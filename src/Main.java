5f7f5dcfeb0449737beed7f325bf7b6c315fc240
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-08-25 12:08:01 +0900

    java start

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+/*   : 범위 주석
+ * // : 한줄 주석
+ * */
+
+/*
+ *  class : 자바 코드를 작성하는 영역
+ *         - 객체의 내용 (속성, 기능)을 정의함
+ *         (JS의 생성자 함수와 비슷한 형태)
+ *         class 이름 = 파일명칭과 동일하게 작성
+ *  */
+public class Main {
+
+  // 메인 메서드, 자바 프로그램을 실행 시키기 위한 구문
+  public static void main(String[] args) {
+    // System.out.println(); : () 안의 문자열을 콘솔에 출력후 줄바꿈
+    System.out.println("Hello World");
+    System.out.println("Java는 코드 끝에 반드시 세미콜론을 붙여야 합니다.");
+    System.out.println(100 + 200);
+    System.out.println(100 + 30 + "입니다");
+    System.out.println("합계: " + (100 + 30) + "원");
+    //https://docs.oracle.com/en/java/javase/21/docs/api/index.html
+  }
+}
