42ae940ab6637a0f05c5c7ff48059ff682679b67
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-08-25 16:30:56 +0900

    operator / 삼항연산자/ Stack/ Heap

--- src/edu/op/ex/OperatorEx.java
+++ src/edu/op/ex/OperatorEx.java
@@ -24,13 +24,51 @@ public class OperatorEx {
   public void method1() {
     Scanner sc = new Scanner(System.in);
     int num = sc.nextInt();
-    boolean isTrue;
-    if (num % 3 == 0) {
-      isTrue = true;
-      System.out.printf("%d는 3의 배수입니다. : %b", num, isTrue);
-    } else {
-      isTrue = false;
-      System.out.printf("%d는 3의 배수가 아닙니다. : %b", num, isTrue);
-    }
+    boolean isTrue = (num % 3 == 0);
+    System.out.printf("%d는 3의 배수입니다. : %b", num, isTrue);
+    System.out.println();
+  }
+
+  /*
+   void는 반환값 X
+   자바라는 언어는 바구니에 들어갈 데이터가 숫자, 문자, 사람이 만든 크기, 실수 등 데이터를 담을 변수이름의 종류부터
+
+   기능을 실행할 때
+   - 기능을 실행만 하면되는 것인지?
+      public void 기능명칭(매개변수, 파라미터 자리){
+      기능
+      }
+   - 기능을 실행 후 실행 결과를 어딘가에 전달하는 기능인지
+      public 전달할자료형 기능명칭(매개변수, 파라미터 자리){
+      기능
+      return 전달할 데이터;
+      }
+   프로그램 수행전에 정의해야할 사항이 많음
+
+   */
+  public void method2() {
+    // Scanner는 1회만 호출후 사용 여러번 가능
+    Scanner sc = new Scanner(System.in);
+    int input1 = sc.nextInt();
+    int input2 = sc.nextInt();
+
+    boolean isTrue = (input1 % input2 == 0);
+
+    System.out.printf("%d는 %d의 배수가 맞는지? %b", input1, input2, isTrue);
+    System.out.println();
+  }
+
+  public void method3() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("정수 입력 :");
+    int input = sc.nextInt();
+
+    //삼항 연산자 : 조건식 ? 참 : 거짓;
+    // if (){} else {} 를 줄인 연산식
+    boolean isTrue = (input % 2 == 0);
+
+    String result = isTrue ? "짝수" : "홀수";
+    System.out.printf("%d은 %s입니다.", input, result);
+
   }
 }
--- src/edu/op/run/OperatorRun.java
+++ src/edu/op/run/OperatorRun.java
@@ -8,6 +8,17 @@ import edu.op.ex.OperatorEx;
 
 public class OperatorRun {
 
+  // static 이 작성된 기능이나 변수이름 메모리에서 프로그램을 사용할 때 상시 사용처럼
+  // 메모리 공간을 차지하는 예약어
+
+  /**
+   * Stack (빠른 비서같은 존재) - 어디에 어떤 기능이 존재하는지 주소만 기억 - 기능(행동) 역할이 종료되면 버리고, 다른 기능을 수행할 준비 - 실질적으로 기능이
+   * 존재하는 주소만 알고 있음
+   * <p>
+   * Heap (창고지기) - Stack에서 주소값으로 연결되어 들어가면 존재하는 실제 물건들의 위치 - 누군가 클래스로 만들어진 객체를 사용하면 보관, 사용하지 않으면
+   * 불필요한 객체나 기능으로 판단하여 버림 - 실제 데이터를 저장하는 공간 (크기 다양)
+   *
+   */
   public static void main(String[] args) {
 
     /**
@@ -22,8 +33,10 @@ public class OperatorRun {
     op.test();
 
     // 기존의 어떤 개발자가 만들어놓은 파일을 가져와서 기능이나 변수값활용시
-    new OperatorEx().test(); //이렇게도 가능은 하다
+    //new OperatorEx().test(); //이렇게도 가능은 하지만 매번 새로운 객체를 생성하기에 지양
 
     op.method1();
+    op.method2();
+    op.method3();
   }
 }
