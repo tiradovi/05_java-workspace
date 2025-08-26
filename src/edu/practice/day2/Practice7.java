53d7c14e62a28956b6935a3b058258d29299ca7c
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-08-26 17:30:32 +0900

    practice .day2 과제

--- src/edu/practice/day2/Practice7.java
+++ src/edu/practice/day2/Practice7.java
@@ -1,5 +1,35 @@
 package edu.practice.day2;
 
-public class Practice7 {
+import java.util.Scanner;
 
+public class Practice7 {
+    Scanner sc = new Scanner(System.in);
+
+    public void method7() {
+        System.out.print("문자열을 입력하세요: ");
+        String str = sc.nextLine();
+        int totalCount = str.length(), letterCount = 0, digitCount = 0, otherCount = 0;
+        char ch;
+        for (int i = 0; i < totalCount; i++) {
+            ch = str.charAt(i);
+
+            /**
+             * Character.isDigit(ch)
+             * Character.isLetter(ch)
+             * 문자가 숫자인지 글자인지 boolean형태로 반환
+             */
+            if (Character.isDigit(ch)) {
+                digitCount++;
+            } else if (Character.isLetter(ch)) {
+                letterCount++;
+            } else {
+                otherCount++;
+            }
+        }
+        System.out.println("=== 문자열 분석 결과===");
+        System.out.println("총 글자 수: " + totalCount);
+        System.out.println("영문자: " + letterCount);
+        System.out.println("숫자: " + digitCount);
+        System.out.println("기타: " + otherCount);
+    }
 }
--- src/edu/practice/day2/PracticeRun.java
+++ src/edu/practice/day2/PracticeRun.java
@@ -3,19 +3,24 @@ package edu.practice.day2;
 public class PracticeRun {
 
     public static void main(String[] args) {
-        Practice1 practice1 = new Practice1();
-        Practice2 practice2 = new Practice2();
-        Practice3 practice3 = new Practice3();
-        Practice4 practice4 = new Practice4();
-        Practice5 practice5 = new Practice5();
-        Practice6 practice6 = new Practice6();
+        Practice1 p1 = new Practice1();
+        Practice2 p2 = new Practice2();
+        Practice3 p3 = new Practice3();
+        Practice4 p4 = new Practice4();
+        Practice5 p5 = new Practice5();
+        Practice6 p6 = new Practice6();
+        Practice7 p7 = new Practice7();
+        Practice8 p8 = new Practice8();
+        Practice9 p9 = new Practice9();
+        Practice10 p10 = new Practice10();
 
-        practice1.method1();
-        practice2.method2();
-        practice3.method3();
-        practice4.method4();
-        practice5.method5();
-        practice6.method6();
+        // p1.method1();
+        // p2.method2();
+        // p3.method3();
+        // p4.method4();
+        // p5.method5();
+        // p6.method6();
+        // p7.method7();
 
 
     }
