b3e228cba41f790903d8f8aaa302fbfd25c7504a
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-08-28 17:07:06 +0900

    클래스 개념

---
+++ src/edu/oop/cls/pack1/ClassTest1.java
@@ -0,0 +1,8 @@
+package edu.oop.cls.pack1;
+
+// (default) : 같은 폴더 패키지 내에서만 접근 가능
+class ClassTest1 {
+    public void method() {
+        System.out.println("classTest1 기능 테스트");
+    }
+}
---
+++ src/edu/oop/cls/pack1/ClassTestRun.java
@@ -0,0 +1,8 @@
+package edu.oop.cls.pack1;
+
+public class ClassTestRun {
+    public static void main(String[] args) {
+        ClassTest1 obj = new ClassTest1();
+        obj.method();
+    }
+}
