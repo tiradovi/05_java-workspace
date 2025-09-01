194d89457784835243e1ad67d51d52838b38e26a
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-09-01 12:11:55 +0900

    상속

---
+++ src/edu/inheritance/개발자.java
@@ -0,0 +1,58 @@
+package edu.inheritance;
+// 상속 : 부모의 코드를 물려 받아 자식이 자신의 것처럼 활용
+
+// 상속 키워드 : extends (확장하다)
+//               -> 부모 코드를 자식이 물려 받아 자식의 전체 크기가 증가
+
+public class 개발자 extends 직원 {
+    // 필드
+    private String computer;
+
+    // 메서드
+    // 기본 생성자
+    public 개발자(String eName, int eId, double baseSalary, String department, String computer) {
+        /*
+          super() : 직원에 존재하는 생성자를 가리킴
+         - 자식 객체 생성시 부모 생성자를 호출하는 생성자
+
+          1) super(): 직원 클래스 안에 존재하는 기본 생성자
+         2) super(매개변수) : 직원 클래스 안에 존재하는 파라미터 생성자 의미
+         */
+        super(eName, eId, baseSalary, department);
+        /*
+        this 대신 super를 사용해서 매개변수로 전달받은 eName, eId, baseSalary, department 사용 처리
+        this.eName = eName;
+        this.eId = eId;
+        this.baseSalary = baseSalary;
+        this.department = department;
+         */
+        this.computer = computer;
+    }
+
+    public String getComputer() {
+        return computer;
+    }
+
+    public void setComputer(String computer) {
+        this.computer = computer;
+    }
+
+    @Override
+    public void info() {
+        super.info();// 직원 클래스에서 기존 info()기능에 작성한 텍스트 호출
+        System.out.println("컴퓨터 : " + getComputer());
+    }
+
+    // 개발자 클래스에서만 다른 기능을 하도록 변경
+    @Override
+    public void work() {
+        System.out.println(geteName() + "개발자가 " + getComputer() + "로 코딩을 합니다.");
+    }
+
+    // 아래는 직원클래스에서 사용하지 않고 개발자 클래스에서만 사용하는 기능 추가
+    public void codeReview() {
+        System.out.println(geteName() + "개발자가 코드 리뷰를 진행합니다.");
+    }
+
+}
+
---
+++ src/edu/inheritance/디자이너.java
@@ -0,0 +1,44 @@
+package edu.inheritance;
+
+public class 디자이너 extends 직원 {
+    /*
+    기존에 하나하나 직접 작성해야하는 필드 명칭에서 공통적으로 활용할 필드 명칭을 작성후
+    extends 직원을 활용하여 추가 필요한 필드 명칭만 작성
+    private String eName;
+    private int eId;
+    private double baseSalary;
+    private String department;
+
+     */
+    private String designTool;
+
+    public 디자이너(String eName, int eId, double baseSalary, String department, String designTool) {
+        super(eName, eId, baseSalary, department);
+        this.designTool = designTool;
+    }
+
+    public String getDesignTool() {
+        return designTool;
+    }
+
+    public void setDesignTool(String designTool) {
+        this.designTool = designTool;
+    }
+
+    /**
+     * @Override 표기는 extends 상속클래스로 가져온 기능을 재활용한다는 표기
+     * 작성하지 않아도 동작하나 컨벤션상 작성
+     */
+    @Override
+    public void info() {
+        super.info();
+        System.out.println("주 사용 디자인 도구:" + getDesignTool());
+    }
+
+    @Override
+    public void work() {
+        //super.work(); 는 부모에 작성된 기능을 추가로 사용하고자 할 때 작성
+        // 즉 필수 아님
+        System.out.println(geteName() + "디자이너가 " + getDesignTool() + "을 활용해 디자인 합니다.");
+    }
+}
