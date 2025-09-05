7aa0d4117a1f0488497100ed269a79e25c5d7604
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-09-05 11:21:35 +0900

    Exception

---
+++ src/edu/exception/pack1/ExceptionEx1.java
@@ -0,0 +1,8 @@
+package edu.exception.pack1;
+
+public class ExceptionEx1 {
+    /*
+    Exception : 코드로 처리 가능한 에러
+    Exception Handling : Exception 발생시 처리하는 방법
+     */
+}
---
+++ src/edu/exception/pack1/ExceptionRun1.java
@@ -0,0 +1,8 @@
+package edu.exception.pack1;
+
+public class ExceptionRun1 {
+    public static void main(String[] args) {
+        ExceptionRun1 exceptionRun1 = new ExceptionRun1();
+
+    }
+}
