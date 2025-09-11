e81fc15b62609f08c8ada671b764c64b666756eb
YOU KI TAE <69350870+tiradovi@users.noreply.github.com>
2025-09-11 12:54:36 +0900

    static final 클래스, 메서드

---
+++ src/edu/variable/ex/직원서비스.java
@@ -0,0 +1,13 @@
+package edu.variable.ex;
+
+import static edu.variable.ex.더조은회사.*;
+
+public class 직원서비스 {
+    public static void 회사정보() {
+
+        System.out.println("회사 : " + 회사이름);
+        System.out.println("주소 : " + 주소);
+        System.out.println("총사원수 : " + 사원수);
+
+    }
+}
